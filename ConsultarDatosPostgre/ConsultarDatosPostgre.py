ac848b438e3c16a7e7816bdc0aa5324e50a1b595
Mixcoatl 120 <mig_ese@hotmail.es>
2023-08-06 18:31:18 -0600

    Hola

--- ConsultarDatosPostgre/ConsultarDatosPostgre.py
+++ ConsultarDatosPostgre/ConsultarDatosPostgre.py
@@ -1,54 +1,59 @@
-from math import e
-from sys import exception
-import pandas as pd
-import openpyxl as op
-from tkinter import filedialog, Tk, ttk, W, E, Canvas, Frame,Button, messagebox
+import tkinter
+import openpyxl
+from tkinter import *
+from tkinter import messagebox, filedialog, Tk, ttk
 
 #funciones
 def cargar_archivo():
-    filename = filedialog.askopenfilename()
     try:
-        openFile = pd.read_excel(filename,engine="openpyxl",sheet_name="Hoja1" )
-        data = openFile.values
-    except(exception):
-        print("Hubo un error inesperado"+ e)
-    finally:
-        print(messagebox.Message("Carga de archivos completa"))
+        filename = filedialog.askopenfilename() #lectura de direccion de archivo(Direccion del archivo)
+        workbook = openpyxl.load_workbook(filename) # lectura de archivo
+        sheet = workbook.active #lectura de la hoja activa 
+        lista_valores = list(sheet.values) #crea una lista de los valores que hay en la hoja activa
 
-    #for i  in data:
-        #print(data)
-        #tabla.insert('',0,)
+        #LLenado de datos 
+        for value_tuple in lista_valores[1:]:
+            tabla.insert('',tkinter.END, values=value_tuple)
 
+        #mesaje de carga completa
+        messagebox.showinfo("Carga completada","Carga completada de archivo excel")
+
+    except:
+        messagebox.showwarning("Error en la carga","Error")
+        
 
 
 
 
 #Inicio de programa
 
+#ventana
 root = Tk()
 root.title("Consulta de tablas")
 
-#canvas
-canvas = Canvas(root, height=600, width=1500)
-canvas.pack()
+#frame1
+frame1 = Frame(root, height=750, width=1500, bg="red")
+frame1.grid(row=0, column=0)
 
-frame = Frame()
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+#frame2
+frame2 = Frame(root, height=100, width=200, bg="blue")
+frame2.grid(row=1, column=0, )
 
-tabla = ttk.Treeview(frame,columns=("Fecha","Bitacora","Razon social","Rfc"))
-tabla.grid(row=1,column=1,columnspan=1,)
-tabla.heading("#0",text="Fecha")
-tabla.heading("#1",text="Bitacora")
-tabla.heading("#2",text="Razon social")
-tabla.heading("#3",text="Rfc")
+#tabla
+tabla = ttk.Treeview(frame1,columns=("Bitacora","Razon social","Rfc"))
+tabla.heading("#0",text="Fecha", anchor=W)
+tabla.heading("#1",text="Bitacora", anchor=W)
+tabla.heading("#2",text="Razon social", anchor=W)
+tabla.heading("#3",text="Rfc", anchor=W)
+tabla.grid(row=1,column=0)
 
-buttonCarga = Button(frame, text="Abrir Excell", command=lambda:cargar_archivo())
-buttonCarga.grid(row=2, column=1)
+#Barra de dislizar de la tabla
+scrollbar = ttk.Scrollbar(frame1, orient=tkinter.VERTICAL, command=tabla.yview)
+tabla.configure(yscroll=scrollbar.set)
+scrollbar.grid(row=1, column=1, sticky='ns')
+
+#boton
+buttonCarga = Button(frame2, text="Abrir Excell", command=lambda:cargar_archivo())
+buttonCarga.grid(row=2, column=0)
 
 root.mainloop()
-
-   
-
-
-#for i in data:
-    #print(i)
